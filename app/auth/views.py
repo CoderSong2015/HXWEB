18890d5058690ac989a343117c8d6d17bc1500b7
Haolin <403438485@qq.com>
2018-04-11 23:52:32 +0800

    finish login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,9 +2,13 @@ __author__ = 'Mrsong'
 
 from . import auth
 from flask import render_template
-
+from .forms import LoginForm
 
 
 @auth.route('/login/' , methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.email.data)
+    return render_template('auth/login.html', form=form)
+
