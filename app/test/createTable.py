29c04824c9cf5c02af8d2c589012d1ffda217371
Haolin <403438485@qq.com>
2018-04-12 23:05:20 +0800

    connection database test

--- app/test/createTable.py
+++ app/test/createTable.py
@@ -7,11 +7,18 @@ db= pymysql.connect(host="localhost",user="root",
     password="123456",db="song",port=3306)
 
 stmt = db.cursor()
-stmt.execute('''
+'''
+stmt.execute(''
              create table if not EXISTS  test (id int not null AUTO_INCREMENT,
                                usrname char(30) not null,
                                email char(100),
                                PRIMARY KEY ( id )
-             );'''
+             );''
              )
+'''
+
+stmt.execute("select * from test")
 db.commit()
+
+rs = stmt.fetchall()
+print(rs)
