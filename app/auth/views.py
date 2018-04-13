01c67483ab044a34dc71dddfb47201b62c660b01
Haolin <403438485@qq.com>
2018-04-13 19:05:32 +0800

    remove useless code

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,22 +12,15 @@ def login():
         print(form.email.data)
         #get user info from database
         user = User(3)
-        print(user)
-        flash('invalid user name or password')
         if user is not None :     # and verify password
             login_user(user, form.remember_me.data)
-            print('login ok!')
-
             next = request.args.get('next')
-            print(next)
             # next_is_valid should check if the user has valid
             # permission to access the `next` url
             ##if not next_is_valid(next):
             ##    return flask.abort(400)
 
             return redirect(next or url_for('main.index'))
-
-        print('go flash')
         flash('invalid user name or password')
     return render_template('auth/login.html', form=form)
 
