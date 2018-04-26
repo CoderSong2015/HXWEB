6a287def7be87f28fe1e18aaa0f8063ee583ab9b
CoderSong2015 <403438485@qq.com>
2018-04-26 16:45:23 +0800

    add home.vue to test

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+const routerOption = [
+  { path: '/', component : 'Home'},
+  { path: '/about', component: 'About'}
+]
+
+const routes = routerOption.map( route => {
+    return {
+      ...route,
+      component: () => import( `@/components/${route.component}.vue`)
+    }
+})
+
 Vue.use(Router)
 
+/*
 export default new Router({
   routes: [
     {
@@ -12,4 +25,9 @@ export default new Router({
       component: HelloWorld
     }
   ]
+})*/
+
+export default new Router({
+  routes,
+  mode: 'history'
 })
